a4d15f0581286907c7c1874179ec4248a00764e7
Axel Wargnier <axel@axessweb.fr>
2018-01-29 13:37:52 +0100

    fix frontal controller

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ if (!file_exists('../vendor/autoload.php')){
 	die();
 }
 
+//autochargement des classes
 require '../vendor/autoload.php';
 
 //config.php créé ?
@@ -13,14 +14,8 @@ if (!file_exists('../app/config.php')){
 	die();
 }
 
-//autochargement des classes
-require '../vendor/autoload.php';
-
 //configuration
 require '../app/config.php';
-
-// rares fonctions globales
-require '../W/globals.php';
 
 //instancie notre appli en lui passant la config et les routes
 $app = new W\App($w_routes, $w_config);
